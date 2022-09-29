b32b6734d431b3a017f6e69fe1d43ac18924ecc4
Meshcheryakov Andrey <31518980+Merakaa@users.noreply.github.com>
2022-09-29 12:02:37 +0500

    $add: обработка событий формы

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const productData = {
+  title: 'test product',
+  price: 13.5,
+  description: 'lorem ipsum set',
+  image: 'https://i.pravatar.cc',
+  category: 'electronic'
+}
 
 export function CreateProduct() {
+  const [value, setValue] = useState('')
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault()
+  }
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  }
+
   return (
-    <form action="">
-      <input type="text" className="border py-2 px-4 mb-2 outline-0 w-full" placeholder='Enter product title...' />
+    <form onSubmit={submitHandler}>
+      <input type="text" className="border py-2 px-4 mb-2 outline-0 w-full" placeholder='Enter product title...'
+        value={value}
+        onChange={changeHandler}
+      />
       <button type='submit' className="py-2 px-4 border bg-yellow-400 hover:text-white">Create</button>
     </form>
   )
